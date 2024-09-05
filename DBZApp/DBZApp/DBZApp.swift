3c22e0419b6ff2ea309d31940c104ace72bd57b9
Oriol Angelet <uriaeri@gmail.com>
2024-09-05 11:03:25 +0200

    Initial commit, main folders created

---
+++ DBZApp/DBZApp/DBZApp.swift
@@ -0,0 +1,17 @@
+//
+//  DBZAppApp.swift
+//  DBZApp
+//
+//  Created by Uri on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DBZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
