536df98653cc220465863b11aa285f0a6ca0acb5
Oriol Angelet <uriaeri@gmail.com>
2024-09-09 16:44:16 +0200

    FilterBarView and logic refactoring

--- DBZApp/DBZApp/Domain/Model/Filter.swift
+++ DBZApp/DBZApp/Domain/Model/Filter.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct Filter: Hashable, Equatable {
     let title: String
+    let subfilters: [String]
     
     static var dbzFilters: [Filter] = [
-        Filter(title: "Affiliation"),
-        Filter(title: "Gender"),
-        Filter(title: "Race")
+        Filter(title: "Affiliation", subfilters: ["Army of Frieza", "Assistant of Beerus", "Assistant of Vermoud", "Freelancer", "Namekian Warrior", "Other", "Pride Troopers", "Red Ribbon Army", "Villain", "Z Fighter"]),
+        Filter(title: "Gender", subfilters: ["Male", "Female", "Other", "Unknown"]),
+        Filter(title: "Race", subfilters: ["Android", "Angel", "Benign Nucleic", "Evil", "Frieza Race", "God", "Human", "Jiren Race", "Majin", "Namekian", "Nucleic", "Saiyan", "Unknown"])
     ]
 }
--- DBZApp/DBZApp/Presentation/Components/FilterBarView.swift
+++ DBZApp/DBZApp/Presentation/Components/FilterBarView.swift
@@ -11,47 +11,60 @@ struct FilterBarView: View {
     var filters: [Filter] = Filter.dbzFilters
     var onXMarkPressed: (() -> Void)? = nil
     var onFilterPressed: ((Filter) -> Void)? = nil
+    var onOptionButtonPressed: (() -> Void)? = nil
     
     // The value is passed by the parent view
     var selectedFilter: Filter? = nil
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                if selectedFilter != nil {
-                    Image(systemName: "xmark")
-                        .padding(8)
-                        .background(
-                            Circle()
-                                .stroke(lineWidth: 1)
-                        )
-                        .foregroundStyle(.dbzBlue)
-                        .background(.black.opacity(0.001))
-                        .onTapGesture {
-                            onXMarkPressed?()
+        HStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    if selectedFilter != nil {
+                        Image(systemName: "xmark")
+                            .padding(8)
+                            .background(
+                                Circle()
+                                    .stroke(lineWidth: 1)
+                            )
+                            .foregroundStyle(.dbzBlue)
+                            .background(.black.opacity(0.001))
+                            .onTapGesture {
+                                onXMarkPressed?()
+                            }
+                            .transition(AnyTransition.move(edge: .leading).combined(with: .opacity))
+                    }
+                    
+                    ForEach(filters, id: \.self) { filter in
+                        if selectedFilter == nil || selectedFilter == filter {
+                            FilterCell(
+                                title: filter.title,
+                                isSelected: selectedFilter == filter
+                            )
+                            .background(.black.opacity(0.001))
+                            .onTapGesture {
+                                onFilterPressed?(filter)
+                            }
+                            .padding(0)
                         }
-                        .transition(AnyTransition.move(edge: .leading).combined(with: .opacity))
-                }
-                
-                ForEach(filters, id: \.self) { filter in
-                    if selectedFilter == nil || selectedFilter == filter {
-                        FilterCell(
-                            title: filter.title,
-                            isSelected: selectedFilter == filter
-                        )
-                        .background(.black.opacity(0.001))
-                        .onTapGesture {
-                            onFilterPressed?(filter)
-                        }
-                        .padding(0)
                     }
                 }
+                .padding(.vertical, 8) // avoids bottom & top of HStack looking cut
+                .padding(.horizontal, 8)
+                .background(.red)
             }
-            .padding(.vertical, 8) // avoids bottom & top of HStack looking cut
-            .padding(.horizontal, 8)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .scrollIndicators(.hidden)
+            .animation(.bouncy, value: selectedFilter)
+            
+            Image(systemName: "slider.horizontal.3")
+                .padding(8)
+                .foregroundStyle(.dbzBlue)
+                .background(.black.opacity(0.001))
+                .onTapGesture {
+                    onOptionButtonPressed?()
+                }
         }
-        .scrollIndicators(.hidden)
-        .animation(.bouncy, value: selectedFilter)
     }
 }
 
