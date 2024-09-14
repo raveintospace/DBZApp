2f353c92d6aca608984ffae4b4dd451cf3a6da0a
Oriol Angelet <uriaeri@gmail.com>
2024-09-14 16:45:51 +0200

    Created enum SortOption

---
+++ DBZApp/DBZApp/Enums/SortOption.swift
@@ -0,0 +1,52 @@
+//
+//  SortOption.swift
+//  DBZApp
+//
+//  Created by Uri on 14/9/24.
+//
+
+import SwiftUI
+
+enum SortOption: String, CaseIterable {
+    case id
+    case idReversed
+    case name
+    case nameReversed
+    case kiPoints
+    case kiPointsReversed
+    
+    func displayName() -> some View {
+        switch self {
+        case .id:
+            return HStack(spacing: 4) {
+                Text("ID")
+                Image(systemName: "chevron.down")
+            }
+        case .idReversed:
+            return HStack(spacing: 4) {
+                Text("ID")
+                Image(systemName: "chevron.up")
+            }
+        case .name:
+            return HStack(spacing: 4) {
+                Text("Name")
+                Image(systemName: "chevron.down")
+            }
+        case .nameReversed:
+            return HStack(spacing: 4) {
+                Text("Name")
+                Image(systemName: "chevron.up")
+            }
+        case .kiPoints:
+            return HStack(spacing: 4) {
+                Text("Neighbourhood")
+                Image(systemName: "chevron.down")
+            }
+        case .kiPointsReversed:
+            return HStack(spacing: 4) {
+                Text("Neighbourhood")
+                Image(systemName: "chevron.up")
+            }
+        }
+    }
+}
