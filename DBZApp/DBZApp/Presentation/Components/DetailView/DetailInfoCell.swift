413c6c808d5281a5c9c0bbf92a037fd87404e8a5
Oriol Angelet <uriaeri@gmail.com>
2024-10-04 16:30:23 +0200

    DetailInfoCell first iteration

---
+++ DBZApp/DBZApp/Presentation/Components/DetailView/DetailInfoCell.swift
@@ -0,0 +1,59 @@
+//
+//  DetailInfoCell.swift
+//  DBZApp
+//
+//  Created by Uri on 4/10/24.
+//
+
+import SwiftUI
+
+struct DetailInfoCell: View {
+    
+    var name: String = Character.mock.name
+    var gender: String = Character.mock.genderToDisplay
+    var kiPoints: String = Character.mock.kiToDisplay
+    var maxKi: String = Character.mock.maxKi
+    var affiliation: String = Character.mock.affiliation
+    var race: String = Character.mock.race
+    var description: String = Character.mock.description
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            HStack(spacing: 0) {
+                Text(name)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(gender)
+                    .font(.title)
+                    .frame(width: 20)
+            }
+
+            Text("Ki points: ")
+                .fontWeight(.semibold) +
+            Text(kiPoints)
+            
+            Text("Max ki points: ")
+                .fontWeight(.semibold) +
+            Text(maxKi)
+            
+            Text("Affiliation: ")
+                .fontWeight(.semibold) +
+            Text(affiliation)
+            
+            Text("Race: ")
+                .fontWeight(.semibold) +
+            Text(race)
+           
+            Text("Description: ")
+                .fontWeight(.semibold) +
+            Text(description)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+#Preview {
+    DetailInfoCell()
+        .padding()
+}
