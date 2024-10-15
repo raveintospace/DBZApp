d034dd89a51fd9c4e8aaaba90bd91306554f9201
Oriol Angelet <uriaeri@gmail.com>
2024-10-15 18:54:07 +0200

    PlanetImageCell accepts zoom, pending to add panning

--- DBZApp/DBZApp/Presentation/Components/PlanetSubviews/PlanetImageCell.swift
+++ DBZApp/DBZApp/Presentation/Components/PlanetSubviews/PlanetImageCell.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PlanetImageCell: View {
     
     var imageName: String = DetailedCharacter.mock.originPlanet.image
-
+    
     @State private var scale: CGFloat = 1.0
     
     var body: some View {
