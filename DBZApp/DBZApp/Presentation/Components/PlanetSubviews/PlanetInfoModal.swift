53ed2765c9d9dc63724189a0c9cb2e212f81f434
Oriol Angelet <uriaeri@gmail.com>
2024-10-24 10:41:15 +0200

    PlanetInfoModal iteration

--- DBZApp/DBZApp/Presentation/Components/PlanetSubviews/PlanetInfoModal.swift
+++ DBZApp/DBZApp/Presentation/Components/PlanetSubviews/PlanetInfoModal.swift
@@ -9,23 +9,58 @@ import SwiftUI
 
 struct PlanetInfoModal: View {
     
+    var title: String = ""
     var name: String = DetailedCharacter.mock.originPlanet.name
     var status: String = DetailedCharacter.mock.originPlanet.planetStatus
     var description: String = DetailedCharacter.mock.originPlanet.translatedPlanetDescription
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 12) {
-            Text(name)
-            Text(status)
-            Text(description)
+        VStack {
+            modalTitle
+                .padding(.bottom, 20)
+            
+            VStack(alignment: .leading, spacing: 12) {
+                nameSection
+                statusSection
+                descriptionSection
+            }
+            .frame(maxWidth: .infinity)
         }
         .foregroundStyle(.accent)
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal)
     }
 }
 
+// posar tot en vstack i donar mes padding bottom a modaltitle
+
 #Preview {
-    PlanetInfoModal()
+    PlanetInfoModal(title: "Planet info")
 }
 
-
+extension PlanetInfoModal {
+    
+    private var modalTitle: some View {
+        Text(title)
+            .font(.largeTitle)
+            .bold()
+            .underline()
+    }
+    
+    private var nameSection: some View {
+        Text("Name: ")
+            .fontWeight(.semibold) +
+        Text(name)
+    }
+    
+    private var statusSection: some View {
+        Text("Status: ")
+            .fontWeight(.semibold) +
+        Text(status)
+    }
+    
+    private var descriptionSection: some View {
+        Text("Description: ")
+            .fontWeight(.semibold) +
+        Text(description)
+    }
+}
