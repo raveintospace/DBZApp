acb3ee42204f55fe309154c56fc623bc84ca3d05
Oriol Angelet <uriaeri@gmail.com>
2024-10-25 19:29:54 +0200

    LandingView first iteration

---
+++ DBZApp/DBZApp/LandingView.swift
@@ -0,0 +1,31 @@
+//
+//  LandingView.swift
+//  DBZApp
+//
+//  Created by Uri on 25/10/24.
+//
+
+import SwiftUI
+import SwiftfulRouting
+
+struct LandingView: View {
+    
+    @Environment(\.router) var router
+    @EnvironmentObject private var homeViewModel: HomeViewModel
+    
+    var body: some View {
+        List {
+            Button("Home") {
+                router.showScreen(.fullScreenCover) { _ in
+                    HomeView()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    RouterView { _ in
+        LandingView()
+    }
+}
