2fd64824206f80a4b0fa16c0b5b4cc894a02ad82
Oriol Angelet <uriaeri@gmail.com>
2024-11-01 18:38:43 +0100

    GameHeaderBar finished

--- DBZApp/DBZApp/Presentation/Components/GameSubviews/GameHeaderBar.swift
+++ DBZApp/DBZApp/Presentation/Components/GameSubviews/GameHeaderBar.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct GameHeaderBar: View {
+    
+    var onHomeButtonPressed: (() -> Void)? = nil
+    var onSettingsButtonPressed: (() -> Void)? = nil
+    
+    @State private var trigger = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            ImageBlueCircle(imageName: "house.fill")
+                .sensoryFeedback(.impact, trigger: trigger)
+                .withTrigger(trigger: $trigger) {
+                    onHomeButtonPressed?()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ImageDBZHeaderLogo()
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            ImageBlueCircle(imageName: "gearshape.fill")
+                .sensoryFeedback(.impact, trigger: trigger)
+                .withTrigger(trigger: $trigger) {
+                    onSettingsButtonPressed?()
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
     }
 }
 
 #Preview {
     GameHeaderBar()
+        .padding(.horizontal)
 }
