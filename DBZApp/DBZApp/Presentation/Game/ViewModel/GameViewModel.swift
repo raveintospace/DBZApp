5434689a638597262ddedb29533b5b84c25bf566
Oriol Angelet <uriaeri@gmail.com>
2024-11-05 19:07:56 +0100

    Created GameViewModel and added method to populate the gameCharacter array using our databaseViewModel

---
+++ DBZApp/DBZApp/Presentation/Game/ViewModel/GameViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewModel.swift
+//  DBZApp
+//
+//  Created by Uri on 5/11/24.
+//
+
+import Foundation
+
+@MainActor
+final class GameViewModel: ObservableObject {
+    
+    @Published var gameCharacters: [GameCharacter] = []
+    
+    private let databaseViewModel: DatabaseViewModel
+    
+    init(databaseViewModel: DatabaseViewModel) {
+        self.databaseViewModel = databaseViewModel
+        Task {
+            await databaseViewModel.loadLocalCharacters()
+            loadGameCharacters()
+        }
+    }
+    
+    private func loadGameCharacters() {
+        self.gameCharacters = databaseViewModel.allCharacters.map( { character in
+            GameCharacterMapper.mapCharacterToGameCharacter(character)
+        })
+    }
+}
