3fac6c1ab04799adf8a44d8573ad99759cbb0161
Oriol Angelet <uriaeri@gmail.com>
2024-11-12 18:01:39 +0100

    GameFooterBar iteration

---
+++ DBZApp/DBZApp/Presentation/Components/GameSubviews/GameFooterBar.swift
@@ -0,0 +1,90 @@
+//
+//  GameFooterBar.swift
+//  DBZApp
+//
+//  Created by Uri on 12/11/24.
+//
+
+import SwiftUI
+
+struct GameFooterBar: View {
+    
+    var leftButtonText: String = ""
+    var leftButtonFigures: String = ""
+    
+    var centerButtonText: String = ""
+    var centerButtonFigures: String = ""
+    
+    var rightButtonText: String = ""
+    var rightbuttonFigures: String = ""
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            roundsRectangle
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            pointsRectangle
+                .frame(maxWidth: .infinity, alignment: .center)
+            
+            currentGameRectangle
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+    }
+}
+
+#Preview {
+    GameFooterBar(
+        leftButtonText: "Round:",
+        leftButtonFigures: "1 / 3",
+        centerButtonText: "Total points:",
+        rightButtonText: "Score:"
+    )
+        .padding()
+}
+
+extension GameFooterBar {
+    private var roundsRectangle: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.dbzBlue)
+            .stroke(.dbzOrange, lineWidth: 4)
+            .frame(width: 100, height: 70)
+            .overlay {
+                VStack {
+                    Text(leftButtonText)
+                    Text(leftButtonFigures)
+                }
+                .foregroundStyle(.dbzYellow)
+                .bold()
+            }
+    }
+    
+    private var pointsRectangle: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.dbzBlue)
+            .stroke(.dbzOrange, lineWidth: 4)
+            .frame(width: 150, height: 70)
+            .overlay {
+                VStack {
+                    Text(leftButtonText)
+                    Text(leftButtonFigures)
+                }
+                .foregroundStyle(.dbzYellow)
+                .bold()
+            }
+    }
+    
+    private var currentGameRectangle: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .fill(.dbzBlue)
+            .stroke(.dbzOrange, lineWidth: 4)
+            .frame(width: 100, height: 70)
+            .overlay {
+                VStack {
+                    Text(leftButtonText)
+                    Text(leftButtonFigures)
+                }
+                .foregroundStyle(.dbzYellow)
+                .bold()
+            }
+    }
+}
