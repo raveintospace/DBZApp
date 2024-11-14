f6e09b34a2faa9f1aebf14285590ffc37cae7c4b
Oriol Angelet <uriaeri@gmail.com>
2024-11-14 12:27:55 +0100

    GameFooterBar iteration

--- DBZApp/DBZApp/Presentation/Components/GameSubviews/GameFooterBar.swift
+++ DBZApp/DBZApp/Presentation/Components/GameSubviews/GameFooterBar.swift
@@ -9,17 +9,17 @@ import SwiftUI
 
 struct GameFooterBar: View {
     
-    var mainButtonText: String = ""
+    var mainButtonText: String = "Total points"
     var mainButtonFigures: String = ""
     
-    var leftButtonText: String = ""
+    var leftButtonText: String = "Game"
     var leftButtonFigures: String = ""
     
-    var centerButtonText: String = ""
+    var centerButtonText: String = "Set"
     var centerButtonFigures: String = ""
     
-    var rightButtonText: String = ""
-    var rightbuttonFigures: String = ""
+    var rightButtonText: String = "Discard"
+    var rightButtonFigures: String = ""
     
     var body: some View {
         VStack {
@@ -27,14 +27,14 @@ struct GameFooterBar: View {
                 pointsRectangle
                     .frame(maxWidth: .infinity, alignment: .center)
             }
-            HStack(spacing: 8) {
-                gameRectangle
+            HStack(spacing: 0) {
+                gamesRectangle
                     .frame(maxWidth: .infinity, alignment: .leading)
                 
                 setGameRectangle
                     .frame(maxWidth: .infinity, alignment: .center)
                 
-                setGameRectangle
+                discardsRectangle
                     .frame(maxWidth: .infinity, alignment: .trailing)
             }
         }
@@ -43,77 +43,29 @@ struct GameFooterBar: View {
 
 #Preview {
     GameFooterBar(
-        leftButtonText: "Game:",
+        mainButtonFigures: "150.000.000.000",
         leftButtonFigures: "1 / 3",
-        centerButtonText: "Total points:",
-        centerButtonFigures: "150.000.000.000",
-        rightButtonText: "Set:",
-        rightbuttonFigures: "R-1 / P-1"
+        centerButtonFigures: "R-1 / P-1",
+        rightButtonFigures: "0 / 3"
     )
         .padding()
 }
 
 extension GameFooterBar {
     private var pointsRectangle: some View {
-        RoundedRectangle(cornerRadius: 15)
-            .fill(.dbzBlue)
-            .stroke(.dbzOrange, lineWidth: 4)
-            .frame(width: 150, height: 70)
-            .overlay {
-                VStack {
-                    Text(centerButtonText)
-                    Text(centerButtonFigures)
-                        .font(.caption)
-                }
-                .foregroundStyle(.dbzYellow)
-                .bold()
-            }
+        GameInfoRectangle(upperText: mainButtonText, lowerText: mainButtonFigures)
     }
     
-    private var gameRectangle: some View {
-        RoundedRectangle(cornerRadius: 15)
-            .fill(.dbzBlue)
-            .stroke(.dbzOrange, lineWidth: 4)
-            .frame(width: 100, height: 70)
-            .overlay {
-                VStack {
-                    Text(leftButtonText)
-                    Text(leftButtonFigures)
-                }
-                .foregroundStyle(.dbzYellow)
-                .bold()
-            }
+    private var gamesRectangle: some View {
+        GameInfoRectangle(upperText: leftButtonText, lowerText: leftButtonFigures)
     }
     
     private var setGameRectangle: some View {
-        RoundedRectangle(cornerRadius: 15)
-            .fill(.dbzBlue)
-            .stroke(.dbzOrange, lineWidth: 4)
-            .frame(width: 100, height: 70)
-            .overlay {
-                VStack {
-                    Text(rightButtonText)
-                    Text(rightbuttonFigures)
-                }
-                .foregroundStyle(.dbzYellow)
-                .bold()
-            }
+        GameInfoRectangle(upperText: centerButtonText, lowerText: centerButtonFigures)
     }
     
     private var discardsRectangle: some View {
-        RoundedRectangle(cornerRadius: 15)
-            .fill(.dbzBlue)
-            .stroke(.dbzOrange, lineWidth: 4)
-            .frame(width: 100, height: 70)
-            .overlay {
-                VStack {
-                    Text(centerButtonText)
-                    Text(centerButtonFigures)
-                        .font(.caption)
-                }
-                .foregroundStyle(.dbzYellow)
-                .bold()
-            }
+        GameInfoRectangle(upperText: rightButtonText, lowerText: rightButtonFigures)
     }
 }
 
