842c358a33bc13c91c95b87a183ed0b5772a893d
Oriol Angelet <uriaeri@gmail.com>
2024-11-15 18:51:35 +0100

    Enum GameText created

---
+++ DBZApp/DBZApp/Enums/GameText.swift
@@ -0,0 +1,40 @@
+//
+//  GameText.swift
+//  DBZApp
+//
+//  Created by Uri on 15/11/24.
+//
+
+import Foundation
+
+enum GameText {
+    case gameWon
+    case gameLost
+    case draw
+    case setWon
+    case setLost
+    case matchWon
+    case matchLost
+    case welcome
+    
+    var message: String {
+        switch self {
+        case .gameWon:
+            return "Game won"
+        case .gameLost:
+            return "Game lost"
+        case .draw:
+            return "Draw"
+        case .setWon:
+            return "Set won"
+        case .setLost:
+            return "Set lost"
+        case .matchWon:
+            return "Match won"
+        case .matchLost:
+            return "Match lost"
+        case .welcome:
+            return "Welcome"
+        }
+    }
+}
