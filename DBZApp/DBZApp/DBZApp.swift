f7130eb9517d82099d2413635b1e88a71ca4af59
Oriol Angelet <uriaeri@gmail.com>
2025-04-14 18:37:00 +0200

    Added AdMob & ATT to main app file

--- DBZApp/DBZApp/DBZApp.swift
+++ DBZApp/DBZApp/DBZApp.swift
@@ -10,8 +10,46 @@ import SwiftfulRouting
 import GoogleMobileAds
 import AppTrackingTransparency
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // AdMob
+        MobileAds.shared.start(completionHandler: nil)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+            self.requestTrackingAuthorization()
+        }
+        
+        return true
+    }
+    
+    // ATT
+    private func requestTrackingAuthorization() {
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                switch status {
+                case .authorized:
+                    debugPrint("Tracking granted")
+                case .denied:
+                    debugPrint("Tracking denied")
+                case .notDetermined:
+                    debugPrint("Tracking not set")
+                case .restricted:
+                    debugPrint("Tracking restricted")
+                @unknown default:
+                    debugPrint("Unknown status")
+                }
+            }
+        }
+    }
+}
+
 @main
 struct DBZApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     @StateObject private var vm = DatabaseViewModel(
         getLocalCharactersUseCase: GetLocalCharactersUseCaseImpl(
